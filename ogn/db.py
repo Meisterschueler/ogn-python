fbbf65f2de4af1b2e08af503bedf4ac120eba3e9
Konstantin Gründger <kammermark@gmx.de>
2015-10-30 20:25:50 +0100

    db creation fixes

--- ogn/db.py
+++ ogn/db.py
@@ -2,14 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from ogn.model.base import Base
+from ogn.model.position import Position
+from ogn.model.receiver import Receiver
+from ogn.model.flarm import Flarm
 
 
 # prepare db
 #engine = create_engine('sqlite:///:memory:', echo=False)
-#engine = create_engine('sqlite:///ogn.db', echo=False)
-engine = create_engine('postgresql://postgres:albatross@localhost:5432/ogn')
+engine = create_engine('sqlite:///ogn.db', echo=False)
+#engine = create_engine('postgresql://postgres:secretpass@localhost:5432/ogn')
 
-Base().metadata.create_all(engine)
+Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
