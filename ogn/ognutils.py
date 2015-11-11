c3066c1d1b6c2622597a300f6f264bc639b6a7f8
Konstantin Gründger <kammermark@gmx.de>
2015-11-11 08:03:42 +0100

    changed device db import/handling

--- ogn/ognutils.py
+++ ogn/ognutils.py
@@ -1,21 +1,39 @@
-from urllib.request import urlopen
+import requests
+import csv
+from io import StringIO
 
-from ogn.model import Flarm
+from .model import Flarm, AddressOrigin
 
 from geopy.geocoders import Nominatim
 
+DDB_URL = "http://ddb.glidernet.org/download"
 
-def get_devices_from_ddb():
+
+def get_ddb(csvfile=None):
+    if csvfile is None:
+        r = requests.get(DDB_URL)
+        rows = '\n'.join(i for i in r.text.splitlines() if i[0] != '#')
+        address_origin = AddressOrigin.ogn_ddb
+    else:
+        r = open(csvfile, 'r')
+        rows = ''.join(i for i in r.readlines() if i[0] != '#')
+        address_origin = AddressOrigin.userdefined
+
+    data = csv.reader(StringIO(rows), quotechar="'", quoting=csv.QUOTE_ALL)
+
     devices = list()
-
-    response = urlopen("http://ddb.glidernet.org/download")
-    lines = response.readlines()
-    for line in lines:
-        if (line.decode()[0] == "#"):
-            continue
-
+    for row in data:
         flarm = Flarm()
-        flarm.parse_ogn(line.decode())
+        flarm.address_type = row[0]
+        flarm.address = row[1]
+        flarm.aircraft = row[2]
+        flarm.registration = row[3]
+        flarm.competition = row[4]
+        flarm.tracked = row[5] == 'Y'
+        flarm.identified = row[6] == 'Y'
+
+        flarm.address_origin = address_origin
+
         devices.append(flarm)
 
     return devices
