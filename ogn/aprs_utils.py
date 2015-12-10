60745fb6f3ce1d499bac23f97b9a80889d2a04ff
Konstantin Gründger <kammermark@gmx.de>
2015-12-10 07:43:18 +0100

    Bugfixed createTimestamp

--- ogn/aprs_utils.py
+++ ogn/aprs_utils.py
@@ -18,20 +18,16 @@ def dmsToDeg(dms):
     return d + m
 
 
-def createTimestamp(hhmmss, reference):
+def createTimestamp(hhmmss, reference=datetime.utcnow(), validate=False):
     hh = int(hhmmss[0:2])
     mm = int(hhmmss[2:4])
     ss = int(hhmmss[4:6])
-
-    if reference is None:
-        reference = datetime.utcnow()
-        return datetime(reference.year, reference.month, reference.day, hh, mm, ss)
 
     if (reference.hour == 23) & (hh == 0):
         reference = reference + timedelta(days=1)
     elif (reference.hour == 0) & (hh == 23):
         reference = reference - timedelta(days=1)
-    elif abs(reference.hour - hh) > 1:
+    elif validate and abs(reference.hour - hh) > 1:
         raise Exception("Time difference is too big. Reference time:%s - timestamp:%s" % (reference, hhmmss))
     return datetime(reference.year, reference.month, reference.day, hh, mm, ss)
 
