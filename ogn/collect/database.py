89b9daf26c0092e105ccfae7b2dcae22dd3c848d
Konstantin Gründger <kammermark@gmx.de>
2016-05-22 07:26:02 +0200

    Changed address origin handling

--- ogn/collect/database.py
+++ ogn/collect/database.py
@@ -1,3 +1,5 @@
+from sqlalchemy.sql import null
+
 from celery.utils.log import get_task_logger
 
 from ogn.model import Device, AddressOrigin
@@ -8,13 +10,42 @@ from ogn.collect.celery import app
 
 logger = get_task_logger(__name__)
 
+temp_address_origin = 7
+
+
+def add_devices(session, origin):
+    before_sq = session.query(Device.address) \
+        .filter(Device.address_origin == origin) \
+        .subquery()
+    add_query = session.query(Device) \
+        .filter(Device.address_origin == temp_address_origin) \
+        .filter(~Device.address.in_(before_sq))
+
+    result = add_query.update({Device.address_origin: origin},
+                              synchronize_session='fetch')
+
+    return result
+
 
 def update_devices(session, origin, devices):
     session.query(Device) \
-        .filter(Device.address_origin == origin) \
+        .filter(Device.address_origin == temp_address_origin) \
         .delete()
 
     session.bulk_save_objects(devices)
+
+    # mark temporary added devices
+    session.query(Device) \
+        .filter(Device.address_origin == null()) \
+        .update({Device.address_origin: temp_address_origin})
+
+    logger.info('Added {} devices'.format(add_devices(session, origin)))
+
+    # delete temporary added devices
+    session.query(Device) \
+        .filter(Device.address_origin == temp_address_origin) \
+        .delete()
+
     session.commit()
 
     return len(devices)
