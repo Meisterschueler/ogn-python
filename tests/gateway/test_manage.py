e8c3e441388ff54f01ff49effa1fe028ced6dd72
dspreitz <dominic@spreitz.de>
2016-09-22 11:13:12 +0000

    Addes test for import_logfile

--- tests/gateway/test_manage.py
+++ tests/gateway/test_manage.py
@@ -2,6 +2,7 @@ import unittest
 import unittest.mock as mock
 
 from ogn.gateway.manage import run
+# from ogn.gateway.manage import import_logfile
 
 
 class GatewayManagerTest(unittest.TestCase):
@@ -16,6 +17,18 @@ class GatewayManagerTest(unittest.TestCase):
         instance.connect.assert_called_once_with()
         self.assertEqual(instance.run.call_count, 1)
         instance.disconnect.assert_called_once_with()
+
+    # try to import stored OGN logfile
+    @mock.patch('ogn.gateway.manage.import_logfile')
+    def test_run_import_logfile(self, mock_import_logfile):
+        # instance = mock_import_logfile.return_value
+
+        # import_logfile(ogn_logfile="tests/OGN_log.txt_2016-09-21", reference_date="2016-09-21")
+
+        # instance.connect.assert_called_once_with()
+        # self.assertEqual(import_logfile.call_count, 1)
+        # instance.disconnect.assert_called_once_with()
+        pass
 
 
 if __name__ == '__main__':
