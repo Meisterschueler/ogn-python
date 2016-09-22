d64a6759e93ec61af31a238784334e92bcbb6f0d
dspreitz <dominic@spreitz.de>
2016-09-22 11:13:12 +0000

    Addes test for import_logfile

--- tests/gateway/test_manage.py
+++ tests/gateway/test_manage.py
@@ -2,7 +2,7 @@ import unittest
 import unittest.mock as mock
 
 from ogn.gateway.manage import run
-
+from ogn.gateway.manage import import_logfile
 
 class GatewayManagerTest(unittest.TestCase):
     # try simple user interrupt
@@ -12,6 +12,19 @@ class GatewayManagerTest(unittest.TestCase):
         instance.run.side_effect = KeyboardInterrupt()
 
         run(aprs_user="testuser")
+
+        instance.connect.assert_called_once_with()
+        self.assertEqual(instance.run.call_count, 1)
+        instance.disconnect.assert_called_once_with()
+
+
+class GatewayManagerTest(unittest.TestCase):
+    # try to import stored OGN logfile
+    @mock.patch('ogn.gateway.manage.import_logfile')
+    def test_run_import_logfile(self, mock_import_logfile)
+        instance = mock_import_logfile.return_value
+
+        import_logfile(ogn_logfile = "OGN_log.txt_2016-09-21")
 
         instance.connect.assert_called_once_with()
         self.assertEqual(instance.run.call_count, 1)
