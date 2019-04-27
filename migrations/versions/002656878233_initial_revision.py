e548611e824680626ca22c7e30e3419ed4e863c9
Konstantin Gründger <kammermark@gmx.de>
2019-04-27 14:28:18 +0200

    Initial revision

---
+++ migrations/versions/002656878233_initial_revision.py
@@ -0,0 +1,24 @@
+"""Initial revision
+
+Revision ID: 002656878233
+Revises: 
+Create Date: 2019-04-27 13:47:29.918982
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '002656878233'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
