cdc44601513f4b80d348bd68d4068f25b9b51ac7
Konstantin Gründger <kammermark@gmx.de>
2020-05-16 00:04:56 +0200

    Added Cache configuration to prevent errors

--- app/config/test.py
+++ app/config/test.py
@@ -2,6 +2,10 @@ SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost:5432/ogn_test"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 
+# Flask-Cache stuff
+CACHE_TYPE = "simple"
+CACHE_DEFAULT_TIMEOUT = 300
+
 # Celery stuff
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
